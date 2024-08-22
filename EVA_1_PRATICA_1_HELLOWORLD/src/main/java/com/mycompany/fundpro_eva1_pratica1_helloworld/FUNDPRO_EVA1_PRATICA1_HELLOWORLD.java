d7d2a84cee40e6f7167e28eadb3b6c81600e4c91
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-08-22 16:24:27 -0600

    Add files via upload

---
+++ EVA_1_PRATICA_1_HELLOWORLD/src/main/java/com/mycompany/fundpro_eva1_pratica1_helloworld/FUNDPRO_EVA1_PRATICA1_HELLOWORLD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fundpro_eva1_pratica1_helloworld;
+
+/**
+ *
+ * @author jesus
+ */
+public class FUNDPRO_EVA1_PRATICA1_HELLOWORLD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
