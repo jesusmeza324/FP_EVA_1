f3da33cfc7c94f9ddcf3cbcad8c7950ed1e0769d
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-08-26 16:52:26 -0600

    Add files via upload

---
+++ EVA_2_VARIABLES/src/main/java/com/mycompany/eva_2_variables/EVA_2_VARIABLES.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_2_variables;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA_2_VARIABLES {
+public static void main(String[]args){
+ System.out.println("HOLA MUNDO!!"); //cadena de texto
+ System.out.println(100); //numero entero
+ System.out.println('c'); //caracter
+ System.out.println(true); //valores logicos
+ System.out.println(false);
+ System.out.println(1000.52); //numero de fraccion (con punto flotante)
+ 
+ //tipos de de datos
+ //ENTEROS
+ //int 
+ //long
+ //byte
+ //short
+ //PUNTO FLOTANTE
+ //Double
+ //float
+ //TEXTO
+ //String
+ //CARACTER
+ //chart
+ //LOGICOS (true false)
+ //bolean
+ //VARIABLES
+ //declaracion:es dar informacion , tipo de dato y el nombre de la variable.
+ //que operaciones debe o puede hacer.
+ //memoria que va utilizar.
+ //EJEMPLO:
+ int edad; //declara una variable de tipo entero llamada edad.
+ //double edad2; //no puede haber variables con el mismo nombre 
+ String nombre;
+ String apellidos;
+ double promedio;
+ //asignacion: poner un valor en la variable.
+ //nombre de la variable=valor;
+ edad = 18; //pongo el 18 dentro de la variable edad.
+
+ 
+ System.out.println(edad); //imprime la variablw
+
+ 
+}
+}
