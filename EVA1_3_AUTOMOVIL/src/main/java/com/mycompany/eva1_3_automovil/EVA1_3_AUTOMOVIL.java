9cc8f1fdf86384596c4b438daa6867f6d3ba04d4
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-08-27 16:50:18 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/main/java/com/mycompany/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+        //declaracion de variables
+       String marca;
+       String modelo;
+       int year;
+       double precio;
+       
+       //asignar valores
+       marca = "Ford";
+       modelo = "Mustang";
+       year = 2022;
+       precio = 999000.1;
+       
+       //imprimir los datos
+       
+       System.out.println(marca); //imprime el contenido de la variable 
+       System.out.println(modelo);
+       System.out.println(year);
+       System.out.println(precio);
+       
+       
+       
+       
+       
+    }
+}
