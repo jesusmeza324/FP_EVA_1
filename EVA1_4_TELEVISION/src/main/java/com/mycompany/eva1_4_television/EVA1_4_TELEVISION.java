e919c116a7b8f0e22a31e9d20f658703caadfdb7
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-08-27 16:50:36 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/main/java/com/mycompany/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_television;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+        String marca;
+        String tecnologia;
+        int pulgadas;
+        double precio;
+        String resolucion;
+        
+        marca = "samsung";
+        tecnologia = "QLED";
+        pulgadas = 55;
+        precio = 16600.50;
+        resolucion = "4K";
+               
+        
+        System.out.println(marca);
+        System.out.println(tecnologia);
+        System.out.println(pulgadas);
+        System.out.println(precio);
+        System.out.println(resolucion);
+        
+        
+        
+    }
+}
