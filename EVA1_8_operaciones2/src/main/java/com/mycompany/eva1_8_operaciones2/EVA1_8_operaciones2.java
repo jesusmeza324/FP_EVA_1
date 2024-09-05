6efaf94daeb12869c08adbf11a60164b12854c93
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-09-04 21:07:17 -0600

    Add files via upload

---
+++ EVA1_8_operaciones2/src/main/java/com/mycompany/eva1_8_operaciones2/EVA1_8_operaciones2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_8_operaciones2;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA1_8_operaciones2 {
+
+    public static void main(String[] args) {
+        String nombre = "jesus";
+        String apellido = "meza";
+        String nombreCom = nombre + " " + apellido;
+        //cadenas de texto, la operacion se llama concanetacion--->unir cadenas de texto
+        //el operador de cocanetacion es el +
+        //cuando un simbolo tiene varias funcioes
+        //se le llama sobrecarga (de operadores)
+        // pregunta de examen + es simbolo de sobrecarga
+        
+        System.out.println("el nombre es: "+nombre);
+        System.out.println("el apellido es: "+apellido);
+        System.out.println("el nombre completo es: "+nombreCom);
+        
+        
+    }
+}
